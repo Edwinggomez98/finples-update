076ff20125e71c24b29543509965171249fb7d25
Edwing Gomez <Edwinggomez98@gmail.com>
2024-10-07 16:32:47 -0400

    updating structure

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './application/app.component';
+import { appConfig } from './application/app.config';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
